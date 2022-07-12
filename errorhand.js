356389d65f9b3954f0a487169ba965a93d0a8613
D N Singh <76054685+DNsingh123-tech@users.noreply.github.com>
2022-07-12 18:59:17 +0530

    Add files via upload

---
+++ errorhand.js
@@ -0,0 +1,41 @@
+console.log("All about error handling")
+
+
+// try block is not showing the error
+try {
+    functionerror()       
+} catch (error) {
+    console.log("you made wrong syntax")
+}
+
+
+try {
+    console.log("you are inside try block");
+    functionerror()       
+} catch (error) {
+    console.log(error)
+    console.log(error.name)
+    console.log(error.message)
+} finally {
+    console.log("finally we will run this");
+}
+
+
+// ( The Statement after throw won't be executed)
+
+// throw 'What the!?'
+// throw 7
+// throw {wut: 'is this'}
+// throw null
+// throw new Promise(() => {})
+// throw undefined
+
+
+// pretend this is comming from a server as a responce
+var a = "D.N.Singh";
+if(a != undefined) {
+    throw new Error("this is defined");
+} else {
+    throw new Error("this is not defined");
+}
+
